2de89e1e33340b8f374c1055d995fdaa08243ec7
Dilan <support@blockhead7360.com>
2021-09-13 01:32:56 -0500

    update search

--- search/search.js
+++ search/search.js
@@ -7,7 +7,7 @@ var checkboxEvents;
 
 function prepare() {
 
-    let info = read("registry.json");
+    let info = read('registry.json');
 
     let jsonData = JSON.parse(info);
 
@@ -15,9 +15,9 @@ function prepare() {
 
     let pr = jsonData.projects;
 
-    checkboxProjects = document.getElementById("searchprojects");
-    checkboxPeople = document.getElementById("searchpeople");
-    checkboxEvents = document.getElementById("searchevents");
+    checkboxProjects = document.getElementById('searchprojects');
+    checkboxPeople = document.getElementById('searchpeople');
+    checkboxEvents = document.getElementById('searchevents');
 
     for (let i = 0; i < pr.length; i++) {
         let item = pr[i];
@@ -31,7 +31,7 @@ function prepare() {
     for (let i = 0; i < pe.length; i++) {
         let item = pe[i];
         data.push(
-            new PersonItem(item.name, item.url, item.role, item.skills)
+            new PersonItem(item.name, item.url, item.role, item.description, item.skills)
         );
     }
 
@@ -39,7 +39,7 @@ function prepare() {
 
 window.onload = function() {
 
-    var res = document.getElementById("searchresults");
+    var res = document.getElementById('searchresults');
 
     for (let i = 0; i < data.length; i++) {
         res.appendChild(data[i].element);
@@ -51,7 +51,7 @@ window.onload = function() {
 
 function search() {
 
-    var inputField = document.getElementById("searchbox");
+    var inputField = document.getElementById('searchbox');
 
     var showAny = false;
 
@@ -61,11 +61,11 @@ function search() {
 
         showAny = true;
 
-        if (inputField.value.includes(",")) {
+        if (inputField.value.includes(',')) {
 
             inp = [];
 
-            let x = inputField.value.split(",");
+            let x = inputField.value.split(',');
             for (let i = 0; i < x.length; i++) {
                 inp[inp.length] = x[i].trim().toLowerCase();
             }
@@ -84,7 +84,7 @@ function search() {
 
         if (!showAny) {
 
-            item.element.style.display = "none";
+            item.element.style.display = 'none';
             continue;
 
         }
@@ -100,14 +100,14 @@ function search() {
 
         if (item instanceof ProjectItem) {
             if (!checkboxProjects.checked) {
-                item.element.style.display = "none";
+                item.element.style.display = 'none';
                 done = true;
             }
             desc = item.description.toLowerCase();
         }
         if (item instanceof PersonItem) {
             if (!checkboxPeople.checked) {
-                item.element.style.display = "none";
+                item.element.style.display = 'none';
                 done = true;
             }
             skills = item.skills;
@@ -118,22 +118,22 @@ function search() {
             for (let y = 0; y < inp.length; y++) {
                 let x = inp[y];
 
-                if (name.includes(x) || type.includes(x) || x === "all") {
-                    item.element.style.display = "";
+                if (name.includes(x) || type.includes(x) || x === 'all') {
+                    item.element.style.display = '';
                     done = true;
                     break;
                 }
 
                 if (desc !== null && desc.includes(x)) {
-                    item.element.style.display = "";
+                    item.element.style.display = '';
                     done = true;
                     break;
                 }
 
                 if (skills !== null) {
                     for (let s = 0; s < skills.length; s++) {
-                        if (skills[s].toLowerCase().includes(x)) {
-                            item.element.style.display = "";
+                        if (allSkills[skills[s]].text.toLowerCase().includes(x)) {
+                            item.element.style.display = '';
                             done = true;
                             break;
                         }
@@ -146,7 +146,7 @@ function search() {
             }
         }
 
-        if (!done) item.element.style.display = "none";
+        if (!done) item.element.style.display = 'none';
 
 
     }
@@ -163,28 +163,28 @@ class ProjectItem {
         this.type = type;
         this.description = description;
 
-        var element = document.createElement("div");
-        element.setAttribute("class", "searchblock");
+        var element = document.createElement('div');
+        element.setAttribute('class', 'searchblock');
 
-        var s1 = document.createElement("span");
-        s1.setAttribute("class", "notscream");
-        s1.textContent = " " + type;
-        var p1 = document.createElement("p");
-        p1.setAttribute("class", "sub");
-        p1.textContent = "PROJECT";
+        var s1 = document.createElement('span');
+        s1.setAttribute('class', 'notscream');
+        s1.textContent = ' ' + type;
+        var p1 = document.createElement('p');
+        p1.setAttribute('class', 'sub');
+        p1.textContent = 'PROJECT';
         p1.appendChild(s1);
         element.appendChild(p1);
 
-        var a2 = document.createElement("a");
-        a2.setAttribute("href", url);
+        var a2 = document.createElement('a');
+        a2.setAttribute('href', url);
         a2.textContent = name;
-        var p2 = document.createElement("p");
-        p2.setAttribute("class", "main");
+        var p2 = document.createElement('p');
+        p2.setAttribute('class', 'main');
         p2.appendChild(a2);
         element.appendChild(p2);
 
-        var p3 = document.createElement("p");
-        p3.setAttribute("class", "moresub");
+        var p3 = document.createElement('p');
+        p3.setAttribute('class', 'moresub');
         p3.textContent = description;
         element.appendChild(p3);
         this.element = element;
@@ -195,70 +195,75 @@ class ProjectItem {
 
 class PersonItem {
 
-    constructor(name, url, type, skills) {
+    constructor(name, url, type, description, skills) {
         this.name = name;
         this.url = url;
         this.type = type;
+        this.description = description;
         this.skills = skills;
 
-        let element = document.createElement("div");
-        element.setAttribute("class", "searchblock");
+        let element = document.createElement('div');
+        element.setAttribute('class', 'searchblock');
 
-        let col1 = document.createElement("div");
-        col1.setAttribute("class", "left");
+        let col1 = document.createElement('div');
+        col1.setAttribute('class', 'left');
 
-        let s1 = document.createElement("span");
-        s1.setAttribute("class", "notscream");
-        s1.textContent = " " + type;
-        let p1 = document.createElement("p");
-        p1.setAttribute("class", "sub");
-        p1.textContent = "PERSON";
+        let s1 = document.createElement('span');
+        s1.setAttribute('class', 'notscream');
+        s1.textContent = ' ' + type;
+        let p1 = document.createElement('p');
+        p1.setAttribute('class', 'sub');
+        p1.textContent = 'PERSON';
         p1.appendChild(s1);
         col1.appendChild(p1);
 
-        let a2 = document.createElement("a");
-        a2.setAttribute("href", url);
+        let a2 = document.createElement('a');
+        a2.setAttribute('href', url);
         a2.textContent = name;
-        let p2 = document.createElement("p");
-        p2.setAttribute("class", "main");
-        p2.appendChild(a2);
-        col1.appendChild(p2);
 
-        let p3 = document.createElement("p");
-        p3.setAttribute("class", "moresub");
-        let txt = "";
+        let nameDiv = document.createElement('div');
+        nameDiv.setAttribute('class', 'search-name');
+
+        let p2 = document.createElement('p');
+        p2.setAttribute('class', 'main');
+        p2.appendChild(a2);
+        nameDiv.appendChild(p2);
+
         for (let i = 0; i < skills.length; i++) {
 
-            txt += skills[i];
+            let skillDiv = document.createElement('div');
+            skillDiv.className = 'hover-wrap';
+            let skillItem = allSkills[skills[i]];
+            //let p = document.createElement('p');
+            //p.setAttribute('class', 'subright');
+            let ic = document.createElement('i');
+            ic.setAttribute('class', 'skill-icon');
+            ic.setAttribute('data-feather', skillItem.icon);
+            ic.setAttribute('width', 20);
+            ic.setAttribute('height', 20);
+            skillDiv.appendChild(ic);
 
-            if (i < skills.length - 1) txt += ", ";
+            let tooltip = document.createElement('div');
+            tooltip.className = 'hover-content';
+            let tooltipP = document.createElement('p');
+            tooltipP.textContent = skillItem.text;
+            tooltip.appendChild(tooltipP);
+            skillDiv.appendChild(tooltip);
+            nameDiv.appendChild(skillDiv);
+            //p.textContent = skillItem.text;
+            //div.appendChild(p);
+            //p2.appendChild(div);
 
         }
-        p3.textContent = txt;
+        col1.appendChild(nameDiv);
+
+        let p3 = document.createElement('p');
+        p3.setAttribute('class', 'moresub');
+        p3.textContent = description;
         col1.appendChild(p3);
 
-        let col2 = document.createElement("div");
-        col2.setAttribute("class", "right");
-
-        for (let i = 0; i < skills.length; i++) {
-
-            let skillItem = allSkills[skills[i]];
-            let div = document.createElement("div");
-            div.setAttribute("class", "iconline");
-            div.setAttribute("style", "display: flex;");
-            let p = document.createElement("p");
-            p.setAttribute("class", "subright");
-            let ic = document.createElement("i");
-            ic.setAttribute("data-feather", skillItem.icon);
-            ic.setAttribute("width", 16);
-            ic.setAttribute("height", 16);
-            ic.setAttribute("style", "padding-right: 8px;")
-            div.appendChild(ic);
-            p.textContent = skillItem.text;
-            div.appendChild(p);
-            col2.appendChild(div);
-
-        }
+        let col2 = document.createElement('div');
+        col2.setAttribute('class', 'right');
 
         element.appendChild(col1);
         element.appendChild(col2);
@@ -275,7 +280,7 @@ function read(file) {
   var result = null;
   var xmlhttp = new XMLHttpRequest();
 
-  xmlhttp.open("GET", file, false);
+  xmlhttp.open('GET', file, false);
   xmlhttp.send();
 
   if (xmlhttp.status == 200) {
