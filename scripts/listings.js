632b698e1d5c89d0c8be813b88f8b0fed5bde76e
Dilan <support@blockhead7360.com>
2021-10-21 01:42:42 -0500

    update header, add listings feature

---
+++ scripts/listings.js
@@ -0,0 +1,95 @@
+
+var listingsDiv = document.getElementById('listing');
+
+function loadListing(file) {
+
+    let data = JSON.parse(read(file));
+
+    let colors = data['colors'];
+
+    let listing = data.listing;
+    for (let i = 0; i < listing.length; i++) {
+
+        let curr = listing[i];
+
+        let lDiv = document.createElement('div');
+        lDiv.id = `listing-div-${i}`;
+        lDiv.className = 'listing-item';
+
+
+        let lButton = document.createElement('button');
+        lButton.id = `listing-button-${i}`;
+        lButton.className = 'main-button';
+        lButton.style.backgroundColor = colors[curr.color];
+
+        lButton.addEventListener('click', () => {
+
+            var button = document.getElementById(`listing-button-${i}`);
+            var next = document.getElementById(`listing-desc-${i}`);
+            if (next.style.maxHeight) {
+                next.style.maxHeight = null;
+            }
+            else {
+                next.style.maxHeight = next.scrollHeight + 'px';
+            }
+            button.classList.toggle('active');
+        })
+
+        let lButton_name = document.createElement('p');
+        lButton_name.className = 'name';
+        lButton_name.innerText = curr.name;
+        lButton.appendChild(lButton_name);
+
+        let lButton_sub = document.createElement('p');
+        lButton_sub.className = 'sub';
+        lButton_sub.innerText = curr.sub;
+        lButton.appendChild(lButton_sub);
+
+        lDiv.appendChild(lButton);
+
+        let lLink = document.createElement('a');
+        lLink.className = 'link-button ' + (curr.button.enabled ? '' : 'disabled');
+        lLink.innerText = curr.button.text;
+        if (curr.button.enabled) {
+            lLink.href = curr.button.link;
+        }
+        lDiv.appendChild(lLink);
+
+        let lInner = document.createElement('div');
+        lInner.id = `listing-desc-${i}`
+        lInner.className = 'inner';
+
+        let lInner_text = document.createElement('p');
+        lInner_text.className = 'text';
+        lInner_text.innerText = curr.desc;
+        lInner.appendChild(lInner_text);
+
+        let lInner_sub = document.createElement('p');
+        lInner_sub.className = 'sub';
+        lInner_sub.innerText = curr.desc_sub;
+        lInner_sub.style.color = colors[curr.color];
+        lInner.appendChild(lInner_sub);
+
+        lDiv.appendChild(lInner);
+
+        listingsDiv.appendChild(lDiv);
+
+    }
+
+
+}
+
+function read(file) {
+
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.open('GET', file, false);
+  xmlhttp.send();
+
+  if (xmlhttp.status == 200) {
+    result = xmlhttp.responseText;
+  }
+
+  return result;
+}
